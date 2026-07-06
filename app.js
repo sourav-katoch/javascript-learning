6e8084762ebe4393135b929e9dd2dd6cde410c24
Sourav <souravkatoch.work@gmail.com>
2026-07-06 14:14:40 +0530

    Completed sping function, used random function

--- app.js
+++ app.js
@@ -74,12 +74,18 @@ const spin = () => {
     }
         const reels = [[], [], []]
         for (let i=0; i < COLS; i++){
-            for(let j = 0; j< ROWS; j++){
-                
+            const reelSymbols = [...symbols];
+             for(let j = 0; j< ROWS; j++){
+                const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+                const selectedSymbol = reelSymbols[randomIndex];
+                reels[i].push(selectedSymbol);
+                reelSymbols.splice(randomIndex, 1);
             }
         }
+        return reels;
 };
-spin();
+const reels = spin();
+console.log(reels);
 const numberOfLines = getNumberOfLines();
 let balance = deposit();
 const bet = getBet(balance, numberOfLines);

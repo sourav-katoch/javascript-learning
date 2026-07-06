bc9363f414f1c87c00e3f0bc7d17b3d5f87262e1
Sourav <souravkatoch.work@gmail.com>
2026-07-06 14:45:49 +0530

    used tranpose function

--- app.js
+++ app.js
@@ -72,8 +72,9 @@ const spin = () => {
             symbols.push(symbol)
         }
     }
-        const reels = [[], [], []]
+    const reels=[];
         for (let i=0; i < COLS; i++){
+            reels.push([]);
             const reelSymbols = [...symbols];
              for(let j = 0; j< ROWS; j++){
                 const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -84,8 +85,23 @@ const spin = () => {
         }
         return reels;
 };
+
+const transpose = (reels) =>{
+    const rows=[];
+    for(let i=0; i<ROWS; i++){
+        rows.push([]);
+        for(let j=0; j<COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    
+    }
+    return rows;
+
+}
 const reels = spin();
+const rows = transpose(reels);
 console.log(reels);
+console.log(rows);
 const numberOfLines = getNumberOfLines();
 let balance = deposit();
 const bet = getBet(balance, numberOfLines);
